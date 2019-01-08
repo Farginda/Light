99631dc482710dcd901d0075733026dd361268fb
Farginda <Farginda@luuks-macbook-pro.local>
2019-01-08 17:38:16 +0100

    fix

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    // if user taps anywhere on the screen, change color to black or white
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
 
+    }
 
 }
+
 
